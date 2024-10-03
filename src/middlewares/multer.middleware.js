c43f7c8d39df9214ed9a8a9615c49c441508bf40
Papan Sarkar <papansrkr876@gmail.com>
2024-10-04 00:59:51 +0530

    bug fix: multer won't store already existing users files anymore ;)

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,27 @@
 import multer from "multer"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+    destination: async function (req, file, cb) {
+        const { username, email } = req.body; // Get username and email from request body
+
+        // Check if the user already exists
+        const existedUser = await User.findOne({
+            $or: [{ username }, { email }]
+        });
+
+        if (existedUser) {
+            // If user exists, do not store the file
+            cb(new ApiError(400, "User already exists, file not stored."));
+        } else {
+            // If user does not exist, store the file
+            cb(null, "./public/temp");
+        }
     },
     filename: function (req, file, cb) {
-      cb(null, file.originalname)
+        cb(null, file.originalname);
     }
-  })
+});
 
-export const upload = multer({ storage })
+export const upload = multer({ storage });
